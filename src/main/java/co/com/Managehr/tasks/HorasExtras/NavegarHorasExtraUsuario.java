c6c611ccfd0dc239730dc6ab9e82b214cf44ac21
sharonlopez19 <sharon.nicolle173@gmail.com>
2025-11-26 20:40:36 -0500

    Horas extra admin correccion

--- src/main/java/co/com/Managehr/tasks/HorasExtras/NavegarHorasExtraUsuario.java
+++ src/main/java/co/com/Managehr/tasks/HorasExtras/NavegarHorasExtraUsuario.java
@@ -7,29 +7,25 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
-
+import static  net.serenitybdd.screenplay.matchers.WebElementStateMatchers.*;
 public class NavegarHorasExtraUsuario implements Task {
-
-    public static NavegarHorasExtraUsuario inicio() {
+    public static NavegarHorasExtraUsuario inicio(){
         return instrumented(NavegarHorasExtraUsuario.class);
     }
-
     @Override
-    public <T extends Actor> void performAs(T actor) {
-        try {
-            actor.attemptsTo(
-                    WaitUntil.the(FormHorasExtra.MENU_HORAS_EXTRA, isVisible())
-                            .forNoMoreThan(30).seconds(),
-                    Click.on(FormHorasExtra.MENU_HORAS_EXTRA),
-                    WaitUntil.the(FormHorasExtra.FORM_CONTENEDOR, isVisible())
-                            .forNoMoreThan(30).seconds()
-            );
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Error no encontró el menú de Horas Extra (vista usuario)");
-        }
+    public <T extends Actor> void performAs(T actor){
+               try {
+                   actor.attemptsTo(
+                           WaitUntil.the(FormHorasExtra.MENU_HORAS_EXTRA, isVisible()).forNoMoreThan(30).seconds(),
+                           Click.on(FormHorasExtra.MENU_HORAS_EXTRA),
+                           WaitUntil.the(FormHorasExtra.FORM_CONTENEDOR, isVisible()).forNoMoreThan(30).seconds()
+                   );
+                   Thread.sleep(3000);
+                   System.out.printf("Se dio click en Horas Extra");
+               } catch (Exception e) {
+                   System.out.printf("No encontro horas extra en el menu");
+               }
+
     }
 }
+
