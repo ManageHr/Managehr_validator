70c63af266d513e5837c81aaadc427ea54ad6652
piter41242 <piter41242@gmail.com>
2025-12-12 00:32:39 -0500

    Reporte contratos parte historia de usuario

---
+++ src/test/java/co/com/Managehr/stepsdefinitions/Contratos/GenerarReporteContratosStepDefinitions.java
@@ -0,0 +1,48 @@
+package co.com.Managehr.stepsdefinitions.Contratos;
+
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
+
+public class GenerarReporteContratosStepDefinitions {
+    @Cuando("^doy clic en \"([^\"]*)\"$")
+    public void doyClicEn(String arg1) {
+
+        
+    }
+
+    @Entonces("^debo visualizar que me encuentro en \"([^\"]*)\"$")
+    public void deboVisualizarQueMeEncuentroEn(String arg1) {
+
+        
+    }
+
+    @Entonces("^se debe visualizar el modal con el reporte$")
+    public void seDebeVisualizarElModalConElReporte() {
+
+        
+    }
+
+    @Cuando("^doy clic en el botón \"([^\"]*)\"$")
+    public void doyClicEnElBotón(String arg1) {
+
+        
+    }
+
+    @Entonces("^se debe descargar el archivo Excel en la carpeta de descargas$")
+    public void seDebeDescargarElArchivoExcelEnLaCarpetaDeDescargas() {
+
+        
+    }
+
+    @Entonces("^se debe descargar un archivo PDF$")
+    public void seDebeDescargarUnArchivoPDF() {
+
+        
+    }
+
+    @Entonces("^el modal debe cerrarse$")
+    public void elModalDebeCerrarse() {
+
+        
+    }
+}
